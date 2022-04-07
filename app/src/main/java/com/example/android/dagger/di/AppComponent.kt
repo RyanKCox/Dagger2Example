4bece6abd4753613ec7f428b5d86dd633a418511
RyanKCox <ryankcox90@gmail.com>
2022-04-07 09:56:12 -0500

    T&C and Details fragment added

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -5,6 +5,8 @@ import com.example.android.dagger.login.LoginActivity
 import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import com.example.android.dagger.settings.SettingsActivity
 import com.example.android.dagger.settings.SettingsComponent
 import dagger.BindsInstance
@@ -27,5 +29,8 @@ interface AppComponent {
     fun settingsComponent():SettingsComponent.Factory
 
     fun loginComponent():LoginComponent.Factory
+
+    fun injection(fragment:EnterDetailsFragment)
+    fun injection(fragment:TermsAndConditionsFragment)
 
 }
